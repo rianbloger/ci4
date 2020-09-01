94adc5bc481c3c1e9fbcb2e610ad7ceabd371c93
rianbloger <rianbloger2@gmail.com>
2020-09-01 22:58:28 +0700

    perbaikan tampilan detail

--- app/Views/komik/detail.php
+++ app/Views/komik/detail.php
@@ -26,9 +26,11 @@
         </div>
         <div class="col-md-8">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title"><?= $komik['judul']; ?></h5>
+                <p class="card-text">Penulis : <?= $komik['penulis']; ?></p>
+                <p class="card-text"><small class="text-muted">Penerbit : <?= $komik['penerbit']; ?></small></p>
+                <div class="btn btn-info">Update</div>
+                <div class="btn btn-danger">Delete</div>
             </div>
         </div>
     </div>
