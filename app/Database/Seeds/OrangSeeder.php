0af8df33671e84b6352f232cb3689f7844980911
rianbloger <rianbloger2@gmail.com>
2020-09-05 08:01:54 +0700

    membuat migrate dan seed

---
+++ app/Database/Seeds/OrangSeeder.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Database\Seeds;
+
+class OrangSeeder extends \CodeIgniter\Database\Seeder
+{
+    public function run()
+    {
+        $data = [
+            'nama'       => 'Rian',
+            'alamat'     => 'Kp cibeureum',
+            'created_at' => ''
+        ];
+
+        // Simple Queries
+        $this->db->query("INSERT INTO orang (nama, alamat) VALUES(:nama:, :alamat:)",
+            $data
+        );
+
+        // Using Query Builder
+//        $this->db->table('users')->insert($data);
+    }
+}
