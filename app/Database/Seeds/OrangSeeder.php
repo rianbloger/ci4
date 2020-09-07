bd451ad821861ef4b4ecfc4048764cde8b911706
rianbloger <rianbloger2@gmail.com>
2020-09-07 08:41:12 +0700

    membuat fungsi faker di bd seeder create 100 orang

--- app/Database/Seeds/OrangSeeder.php
+++ app/Database/Seeds/OrangSeeder.php
@@ -25,22 +25,23 @@ class OrangSeeder extends \CodeIgniter\Database\Seeder
 //            'updated_at' => Time::now()
 //        ]];
 
-        $faker = \Faker\Factory::create();
-
+        $faker = \Faker\Factory::create('id_ID');
+        for ($i=0; $i <= 100; $i++){
         $data =[
             'nama'       => $faker->name,
             'alamat'     => $faker->address,
-            'created_at' => Time::now(),
+            'created_at' => Time::createFromTimestamp($faker->unixTime),
             'updated_at' => Time::now()
         ];
-
+        $this->db->table('orang')->insert($data);
+        }
         // Simple Queries
         // $this->db->query("INSERT INTO orang (nama, alamat, created_at, updated_at) VALUES(:nama:, :alamat:, :created_at:, :updated_at:)",
         //     $data
         // );
 
         // Using Query Builder
-        $this->db->table('orang')->insert($data);
+
 //       $this->db->table('orang')->insertBatch($data);
     }
 }
